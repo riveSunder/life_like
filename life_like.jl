03fc30a15274f8d2212306b3bbbff70b040f11a0
RiveSunder <rivesunder@protonmail.com>
2021-08-22 19:05:44 -0600

    update ca functions

--- life_like.jl
+++ life_like.jl
@@ -81,8 +81,7 @@ end
 
 function ft_convolve(grid, kernel)
     
-    grid2 = grid #pad_to_2d(grid, size(grid) .* 2)
-    #grid2 = pad_circular(grid)
+    grid2 = grid 
     
     if size(grid2) != size(kernel)
         padded_kernel = pad_to_2d(kernel, size(grid2))
@@ -90,17 +89,14 @@ function ft_convolve(grid, kernel)
         padded_kernel = kernel
     end
     
-    abs.(FFTW.ifftshift(FFTW.ifft(FFTW.fft(FFTW.fftshift(uni.grid)) .* FFTW.fft(temp) ) ))
+    abs.(FFTW.ifftshift(FFTW.ifft(FFTW.fft(FFTW.fftshift(grid)) .* FFTW.fft(padded_kernel) ) ))
 
     convolved = round.(FFTW.ifftshift(abs.(FFTW.ifft(
                     FFTW.fft(FFTW.fftshift(grid2)) .* 
                     FFTW.fft(FFTW.fftshift(padded_kernel)) ))) )
 
-    #new_convolved = pad_to_2d(convolved[2:size(grid)[1], 2:size(grid)[2]], size(grid)) #zeros(size(convolved))
     
-    new_convolved = convolved[2:size(convolved)[1]-1, 2:size(convolved)[1]-1]
-   
-    return convolved #convolved[1:size(grid)[1], 1:size(grid)[2]]
+    return convolved 
 end
 
 function ca_update(grid, rules)
@@ -109,26 +105,35 @@ function ca_update(grid, rules)
     kernel = ones(Int32, 3,3)
     kernel[2, 2] = 0
     
-    moore_grid = ft_convolve(pad_circular(grid), kernel)
-
-    moore_grid = moore_grid[2:33, 2:33]
-    new_grid = zeros(size(moore_grid))
+    moore_grid = ft_convolve(grid, kernel)
     
+    new_grid = zeros(size(moore_grid))
+   
+    #my_fn(a,b) = a + b
+    #born = reduce(my_fn, [elem .== moore_grid for elem in rules[1]])
+    #live = reduce(my_fn, [elem .== moore_grid for elem in rules[2]])
+
+    #new_grid = grid .* born + (1 .- grid) * live
+ 
     for birth in rules[1]
-        new_grid[(round.(moore_grid .- birth) .== 0.0) .& (grid .!= 1)] .= 1
+        #new_grid[(round.(moore_grid .- birth) .== 0.0) .& (grid .!= 1)] .= 1
+        new_grid[((moore_grid .== birth) .& (grid .!= 1))] .= 1
     end
 
     for survive in rules[2]
-        new_grid[(round.(moore_grid .- survive) .== 0.0) .& (grid .== 1)] .= 1
+        #new_grid[(round.(moore_grid .- survive) .== 0.0) .& (grid .== 1)] .= 1
+        new_grid[((moore_grid .== survive) .& (grid .== 1))] .= 1
     end
 
     return new_grid 
 end
 
 
-function ca_step(universe::Universe)
-    
-    universe.grid = ca_update(universe.grid, [universe.born, universe.live]) 
+function ca_steps(universe::Universe, steps::Int64)
+
+    for ii = 1:steps
+        universe.grid = ca_update(universe.grid, [universe.born, universe.live]) 
+    end
 
 end
 
